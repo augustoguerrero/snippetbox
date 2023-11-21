0050f8eb8b626dc12b2c2e830d5876d03270db74
augustoguerrero <augusto.codex@gmail.com>
2023-11-21 16:50:23 -0300

    updated dockerfile
    Signed-off-by: augustoguerrero <augusto.codex@gmail.com>

--- internal/models/errors.go
+++ internal/models/errors.go
@@ -5,13 +5,7 @@ import (
 )
 
 var (
-	ErrNoRecord = errors.New("models: no matching record found")
-
-	// Add a new ErrInvalidCredentials error. We'll use this later if a user
-	// tries to login with an incorrect email address or password.
+	ErrNoRecord           = errors.New("models: no matching record found")
 	ErrInvalidCredentials = errors.New("models: invalid credentials")
-
-	// Add a new ErrDuplicateEmail error. We'll use this later if a user
-	// tries to signup with an email address that's already in use.
-	ErrDuplicateEmail = errors.New("models: duplicate email")
+	ErrDuplicateEmail     = errors.New("models: duplicate email")
 )
